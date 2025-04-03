8f71b84564c4e95501bbab5244134164afa1b1b0
Gage Smith <gsmith@gntsecurity.com>
2025-04-03 14:25:59 -0400

    UX

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { supabase } from '../../utils/supabaseClient'
+import supabase from '../../utils/supabaseClient'
 
 export default function TestPage() {
   const [tables, setTables] = useState<string[]>([])
